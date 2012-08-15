cd7ad1308163718c398d94cc2162c74aef03fb05
jaedong27 <jaedong27@gmail.com>
2012-08-15 17:09:42 +0900

    Server First Commit

---
+++ Server/SmartCubeServer/frmSmartCube.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace SmartCubeServer
+{
+    partial class frmSmartCube
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(766, 519);
+            this.webBrowser.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(643, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmSmartCube
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(766, 519);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webBrowser);
+            this.Name = "frmSmartCube";
+            this.Text = "SmartCube";
+            this.Load += new System.EventHandler(this.frmSmartCube_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Server/SmartCubeServer/frmSmartCube.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using Bend.Util;
+
+namespace SmartCubeServer
+{
+    public partial class frmSmartCube : Form
+    {
+        public frmSmartCube()
+        {
+            InitializeComponent();
+        }
+
+        private void frmSmartCube_Load(object sender, EventArgs e)
+        {
+            //Server Start
+            HttpServer httpServer;
+            httpServer = new MyHttpServer(8080);
+            Thread thread = new Thread(new ThreadStart(httpServer.listen));
+            thread.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser.Navigate("http://localhost:8080");
+        }
+    }
+}
